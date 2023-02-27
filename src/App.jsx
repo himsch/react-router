92536ef44a05f90e37f6e94972eb13cf1c146be9
song <55rsearchco@gmail.com>
2023-02-27 15:46:00 +0900

    react router 설치 및 기본 사용법

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <p>Home</p>,
+    errorElement: <p>Not Found😱</p>,
+  },
+  {
+    path: 'videos',
+    element: <p>Videos</p>,
+  },
+]);
 
 function App() {
-  return <p>App</p>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
